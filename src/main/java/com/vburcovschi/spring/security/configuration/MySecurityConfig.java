a532bb5c284bf23e52651cf0f8b36e82a6adf633
vburcovschi <68415281+vburcovschi@users.noreply.github.com>
2023-06-06 10:56:49 +0300

    Authorisation

--- src/main/java/com/vburcovschi/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/vburcovschi/spring/security/configuration/MySecurityConfig.java
@@ -1,6 +1,7 @@
 package com.vburcovschi.spring.security.configuration;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -15,5 +16,14 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser(userBuilder.username("vitalie").password("vitalie").roles("EMPLOYEE"))
                 .withUser(userBuilder.username("elena").password("elena").roles("HR"))
                 .withUser(userBuilder.username("boss").password("boss").roles("MANAGER","HR"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("EMPLOYEE","HR","MANAGER")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manager_info").hasRole("MANAGER")
+                .and().formLogin().permitAll();
     }
 }
--- src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
+++ src/main/webapp/WEB-INF/view/view_for_all_employees.jsp
@@ -1,14 +1,19 @@
+<%@ taglib prefix="security" uri="http://www.springframework.org/security/tags" %>
 <!DOCTYPE html>
 <html>
 <body>
 
 <h3>Information for all Employees</h3>
 <br><br>
+<security:authorize access="hasRole('MANAGER')">
 <input type="button" value="Salary" onclick="window.location.href = 'hr_info'">
 Only for HR staf
+</security:authorize>
 <br><br>
+<security:authorize access="hasRole('HR')">
 <input type="button" value="Performance" onclick="window.location.href = 'manager_info'">
 Only for Manager staf
+</security:authorize>
 
 </body>
 </html>
