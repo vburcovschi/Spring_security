2d63b2aa6704d42d4a3e920ea3fb0b014759bff2
vburcovschi <68415281+vburcovschi@users.noreply.github.com>
2023-06-06 11:11:25 +0300

    No encrypted passwords in DB

--- src/main/java/com/vburcovschi/spring/security/configuration/MyConfig.java
+++ src/main/java/com/vburcovschi/spring/security/configuration/MyConfig.java
@@ -1,10 +1,14 @@
 package com.vburcovschi.spring.security.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 @ComponentScan("com.vburcovschi.spring.security")
@@ -16,4 +20,18 @@ public class MyConfig {
         internalResourceViewResolver.setSuffix(".jsp");
         return internalResourceViewResolver;
     }
+    @Bean
+    public DataSource dataSource(){
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false");
+            dataSource.setUser("root");
+            dataSource.setPassword("root");
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+        return dataSource;
+    }
+
 }
--- src/main/java/com/vburcovschi/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/vburcovschi/spring/security/configuration/MySecurityConfig.java
@@ -1,5 +1,6 @@
 package com.vburcovschi.spring.security.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -7,15 +8,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
 
+import javax.sql.DataSource;
+
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+/*        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
         auth.inMemoryAuthentication()
                 .withUser(userBuilder.username("vitalie").password("vitalie").roles("EMPLOYEE"))
                 .withUser(userBuilder.username("elena").password("elena").roles("HR"))
-                .withUser(userBuilder.username("boss").password("boss").roles("MANAGER","HR"));
+                .withUser(userBuilder.username("boss").password("boss").roles("MANAGER","HR"));*/
+
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Override
